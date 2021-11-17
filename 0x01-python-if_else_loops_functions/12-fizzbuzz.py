02a23724f9a7fc27e2bdc6a9068a62379e24f5a2
Ambrose23456 <onyemakonorambrose@gmail.com>
2021-11-17 12:53:26 -0800

    fix

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzBuzz():
     """the famous fizzBuzz challenge. Here a range of numbers from 1 to 100 
        is printed and along the way at designated points "fizz", "Buzz" or 
         Fizzbuzz" is printed instead of the number in the sequence"""
-    for i in range(1,101):
+    for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz ", end="")
         elif i % 3 == 0:
