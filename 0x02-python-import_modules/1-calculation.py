2f7b18dd6201fe6d384bd4fe767127f71f12ebd1
Ambrose23456 <onyemakonorambrose@gmail.com>
2021-11-21 19:56:50 -0800

    fix 1-calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """performs add, subtract, divide and multiplication on given numbers"""
-from calculator_1 import sub, mul, div, add 
-a = 10
-b = 5
+if __name__ == "__main__":
+    from calculator_1 import sub, mul, div, add
 
-print("{} + {} = {}".format(a,b, add(a,b)))
+    a = 10
+    b = 5
 
-print("{} - {} = {}".format(a,b, sub(a,b)))
-
-print("{} * {} = {}".format(a,b, mul(a,b)))
-
-print("{} / {} = {}".format(a,b, div(a,b)))
+    print("{} + {} = {}".format(a,b, add(a,b)))
+    print("{} - {} = {}".format(a,b, sub(a,b)))
+    print("{} * {} = {}".format(a,b, mul(a,b)))
+    print("{} / {} = {}".format(a,b, div(a,b)))
